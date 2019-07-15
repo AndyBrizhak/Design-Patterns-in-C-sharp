2c1c0423b86750347514332026268ce15beae095
AndyBrizhak <andrewbrizhak@gmail.com>
2019-07-15 09:05:47 +0800

    +

---
+++ BuilderInheritance/Creational.Builder.BuilderInheritance.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DotNetDesignPatternDemos.Creational.Builder
+{
+  public class Person
+  {
+    public string Name;
+
+    public string Position;
+
+    class Builder : PersonInfoBuilder<Builder> { /* degenerate */ }
+
+    public override string ToString()
+    {
+      return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+    }
+  }
+
+  abstract class PersonBuilder<SELF>
+    where SELF : PersonBuilder<SELF>
+  {
+    protected Person person = new Person();
+
+    public Person Build()
+    {
+      return person;
+    }
+  }
+
+  class PersonInfoBuilder<SELF> : PersonBuilder<PersonInfoBuilder<SELF>>
+    where SELF : PersonInfoBuilder<SELF>
+  {
+    public SELF Called(string name)
+    {
+      person.Name = name;
+      return (SELF) this;
+    }
+  }
+
+  class PersonJobBuilder<SELF> : PersonInfoBuilder<PersonJobBuilder<SELF>>
+    where SELF : PersonJobBuilder<SELF>
+  {
+    public SELF WorksAsA(string position)
+    {
+      person.Position = position;
+      return (SELF) this;
+    }
+  }
+
+  public class BuilderInheritanceDemo
+  {
+    static void Main(string[] args)
+    {
+      var builder = new Builder();
+      var person = builder.WorksAsA("d").
+      Console.WriteLine(person);
+    }
+  }
+}
---
+++ Life without Builder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life_without_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = "Hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb);
+        }
+    }
+}
