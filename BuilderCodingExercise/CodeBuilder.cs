72779c5fc7abcb1970b0883cc04787037d321828
AndyBrizhak <andrewbrizhak@gmail.com>
2019-07-17 07:30:06 +0800

    + корректный вариант класса CodeBuilder

--- BuilderCodingExercise/CodeBuilder.cs
+++ BuilderCodingExercise/CodeBuilder.cs
@@ -1,11 +1,9 @@
 ﻿using System.Collections.Generic;
-
-using System.Collections.Generic;
 using System.Text;
-using System;
 
 namespace BuilderCodingExercise
 {
+
     public class CodeBuilder
    {
        public string ClassName;
