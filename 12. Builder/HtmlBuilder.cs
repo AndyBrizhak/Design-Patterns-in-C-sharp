34504125dd7bb68b8f3488c2651dc4e0f2dc1e67
AndyBrizhak <andrewbrizhak@gmail.com>
2019-07-16 09:14:55 +0800

    +

---
+++ 12. Builder/HtmlBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Builder
+{
+    class HtmlBuilder
+  {
+    private readonly string rootName;
+
+    public HtmlBuilder(string rootName)
+    {
+      this.rootName = rootName;
+      root.Name = rootName;
+    }
+
+    // not fluent
+    public void AddChild(string childName, string childText)
+    {
+      var e = new HtmlElement(childName, childText);
+      root.Elements.Add(e);
+    }
+
+    public HtmlBuilder AddChildFluent(string childName, string childText)
+    {
+      var e = new HtmlElement(childName, childText);
+      root.Elements.Add(e);
+      return this;
+    }
+
+    public override string ToString()
+    {
+      return root.ToString();
+    }
+
+    public void Clear()
+    {
+      root = new HtmlElement{Name = rootName};
+    }
+
+    HtmlElement root = new HtmlElement();
+  }
+}
