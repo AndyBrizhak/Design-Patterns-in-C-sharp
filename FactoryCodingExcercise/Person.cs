b195c1ff87f5f12d3839e353ab329b1667562991
AndyBrizhak <andrewbrizhak@gmail.com>
2019-07-19 19:57:35 +0800

    +

---
+++ FactoryCodingExcercise/Person.cs
@@ -0,0 +1,13 @@
+namespace FactoryCodingExcercise
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+//        public override string ToString()
+//        {
+//            return base.ToString();
+//        }
+    }
+}
---
+++ FactoryCodingExcercise/PersonFactory.cs
@@ -0,0 +1,16 @@
+namespace FactoryCodingExcercise
+{
+    public class PersonFactory
+    {
+        private static int _index = 0;
+        
+        public static Person CreatePerson(string name)
+        {
+            var p = new Person();
+            p.Id = _index;
+            p.Name = name;
+            _index++;
+            return p;
+        }
+    }
+}
