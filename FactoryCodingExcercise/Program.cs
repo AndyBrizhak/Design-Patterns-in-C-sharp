768af02d38bc6e68d960ae4ba12a5ea62b0039fa
AndyBrizhak <andrewbrizhak@gmail.com>
2019-07-19 19:58:19 +0800

    +

---
+++ FactoryCodingExcercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FactoryCodingExcercise
+{
+    public static class Program
+    {
+        
+        
+        public static void Main(string[] args)
+        {
+            
+            var person1 = PersonFactory.CreatePerson("Vasia");
+            Console.WriteLine(person1);
+            Console.WriteLine(person1.Id);
+            Console.WriteLine(person1.Name);
+            Console.ReadKey();
+            var person2 = PersonFactory.CreatePerson("Ivan");
+            Console.WriteLine(person2);
+            Console.WriteLine(person2.Id);
+            Console.WriteLine(person2.Name);
+            Console.ReadKey();
+            
+            
+        }
+    }
+}
