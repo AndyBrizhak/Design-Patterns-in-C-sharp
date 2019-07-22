fbd9e478718836be4009673a647797903f7efa70
AndyBrizhak <andrewbrizhak@gmail.com>
2019-07-22 16:22:47 +0800

    working correct!

---
+++ Adapter.Coding.Exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Adapter.Coding.Exercise
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sq = new Square();
+            sq.Side = 2;
+            var rec = new SquareToRectangleAdapter(square:sq);
+            var area = rec.Area();
+            Console.WriteLine(area);
+            Console.ReadKey();
+        }
+    }
+}
