0f7d0f416325fb19e04c7b81b03f0b733fcb5e3e
AndyBrizhak <andrewbrizhak@gmail.com>
2019-07-22 15:16:33 +0800

    working correct!

--- Adapter.Coding.Exercise/SquareToRectangleAdapter.cs
+++ Adapter.Coding.Exercise/SquareToRectangleAdapter.cs
@@ -1,10 +1,19 @@
 namespace Adapter.Coding.Exercise
 {
-    public class SquareToRectangleAdapter
+    public class SquareToRectangleAdapter : IRectangle
     {
         public SquareToRectangleAdapter(Square square)
         {
+            Width = square.Side;
+            Height = square.Side;
+        }
+
+        public SquareToRectangleAdapter(int width, int height)
+        {
             
         }
+
+        public int Width { get; }
+        public int Height { get; }
     }
 }
