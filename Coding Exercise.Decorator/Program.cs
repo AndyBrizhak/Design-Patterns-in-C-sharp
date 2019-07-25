5999e2b55117c51a43d23f42b3e5efa06ef1a3e5
AndyBrizhak <andrewbrizhak@gmail.com>
2019-07-25 18:58:28 +0800

    +

---
+++ Coding Exercise.Decorator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Coding_Exercise.Decorator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dragon = new Dragon();
+            dragon.Age = 3;
+            Console.WriteLine(dragon);
+            Console.ReadKey();
+        }
+    }
+}
