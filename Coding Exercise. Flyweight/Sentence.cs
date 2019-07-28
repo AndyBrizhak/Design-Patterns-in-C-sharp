8e5aded8e6f3fb8d7aa59d719d3ba1dfae304f6a
AndyBrizhak <andrewbrizhak@gmail.com>
2019-07-28 13:07:20 +0800

    +Flyweight done....

--- Coding Exercise. Flyweight/Sentence.cs
+++ Coding Exercise. Flyweight/Sentence.cs
@@ -1,23 +1,39 @@
+using System.Collections.Generic;
+
 namespace Coding_Exercise._Flyweight
 {
     public class Sentence
     {
+        private string[] words;
+        private Dictionary<int, WordToken> tokens = new Dictionary<int, WordToken>();
+        
         public Sentence(string plainText)
         {
-            // todo
+             words = plainText.Split(' ');
         }
     
         public WordToken this[int index]
         {
             get
             {
-                // todo
+               WordToken wt = new WordToken();
+               tokens.Add(index, wt);
+               return tokens[index];
             }
         }
     
         public override string ToString()
         {
             // output formatted text here
+            var ws = new List<string>();
+            for (var i = 0; i < words.Length; i++)
+                    {
+                      var w = words[i];
+                      if (tokens.ContainsKey(i) && tokens[i].Capitalize)
+                        w = w.ToUpperInvariant();
+                      ws.Add(w);
+                    }
+            return string.Join(" ", ws);
         }
     
         public class WordToken
