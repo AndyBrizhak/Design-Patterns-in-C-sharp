0a1293e35faa06e586db310cefb7966c09623508
AndyBrizhak <andrewbrizhak@gmail.com>
2019-07-30 12:45:50 +0800

    +test

---
+++ Coding Exercise. ResponsiblePerson/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Coding_Exercise._ResponsiblePerson
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            var person = new Person();
+            person.Age = 15;
+            
+            var  driver= new ResponsiblePerson(person);
+            
+            Console.WriteLine(driver.DrinkAndDrive());
+            
+        }
+    }
+}
