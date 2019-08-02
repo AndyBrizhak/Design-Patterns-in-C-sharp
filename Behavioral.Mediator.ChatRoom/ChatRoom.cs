1a864c10fceaf4fd74fe889c78b83201d6efce1d
AndyBrizhak <andrewbrizhak@gmail.com>
2019-08-02 12:16:05 +0800

    Mediator. ChatRoom

---
+++ Behavioral.Mediator.ChatRoom/ChatRoom.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Behavioral.Mediator.ChatRoom
+{
+    public class ChatRoom
+    {
+        private List<Person> people = new List<Person>();
+
+        public void Broadcast(string source, string message)
+        {
+            foreach (var p in people)
+                if (p.Name != source)
+                    p.Receive(source, message);
+        }
+
+        public void Join(Person p)
+        {
+            string joinMsg = $"{p.Name} joins the chat";
+            Broadcast("room", joinMsg);
+
+            p.Room = this;
+            people.Add(p);
+        }
+
+        public void Message(string source, string destination, string message)
+        {
+            people.FirstOrDefault(p => p.Name == destination)?.Receive(source, message);
+        }
+    }
+}
---
+++ Behavioral.Mediator.ChatRoom/Person.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+
+namespace Behavioral.Mediator.ChatRoom
+{
+    public class Person
+    {
+        public string Name;
+        public ChatRoom Room;
+        private List<string> chatLog = new List<string>();
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void Receive(string sender, string message)
+        {
+            string s = $"{sender}: '{message}'";
+            WriteLine($"[{Name}'s chat session] {s}");
+            chatLog.Add(s);
+        }
+
+        private void WriteLine(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Say(string message)
+        {
+            Room.Broadcast(Name, message);
+        }
+
+        public void PrivateMessage(string who, string message)
+        {
+            Room.Message(Name, who, message);
+        }
+    }
+}
