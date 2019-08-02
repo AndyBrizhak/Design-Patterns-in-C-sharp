5f117638ca6a90c38a810d3edcd76952ae56a9d7
AndyBrizhak <andrewbrizhak@gmail.com>
2019-08-02 15:02:28 +0800

    Mediator. ChatRoom

---
+++ Behavioral.Mediator.ChatRoom/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Behavioral.Mediator.ChatRoom
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var room = new ChatRoom();
+            var john = new Person("John");
+            var jane = new Person("Jane");
+        }
+    }
+}
