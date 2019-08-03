bcb0f26394c117840f379495377df1d80b79e3c4
AndyBrizhak <andrewbrizhak@gmail.com>
2019-08-03 13:03:22 +0800

    +

---
+++ RxEventBroker/Program.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+
+namespace RxEventBroker
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cb = new ContainerBuilder();
+            cb.RegisterType<EventBroker>().SingleInstance();
+            cb.RegisterType<FootballCoach>();
+            cb.RegisterType<Ref>();
+            cb.Register((c, p) => new FootballPlayer(c.Resolve<EventBroker>(), p.Named<string>("name")));
+
+            using (var c = cb.Build())
+            {
+                var referee = c.Resolve<Ref>(); // order matters here!
+                var coach = c.Resolve<FootballCoach>();
+                var player1 = c.Resolve<FootballPlayer>(new NamedParameter("name", "John"));
+                var player2 = c.Resolve<FootballPlayer>(new NamedParameter("name", "Chris"));
+                player1.Score();
+                player1.Score();
+                player1.Score(); // only 2 notifications
+                player1.AssaultReferee();
+                player2.Score();
+            }
+        }
+    }
+}
