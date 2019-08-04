61baf024feafca57d27df221af70c76a9ab635db
AndyBrizhak <andrewbrizhak@gmail.com>
2019-08-04 09:38:56 +0800

    +Behavioral.Memento

---
+++ Behavioral.Memento/BankAccount.cs
@@ -0,0 +1,28 @@
+namespace Behavioral.Memento
+{
+    public class BankAccount
+    {
+        private int balance;
+
+        public BankAccount(int balance)
+        {
+            this.balance = balance;
+        }
+
+        public Memento Deposit(int amount)
+        {
+            balance += amount;
+            return new Memento(balance);
+        }
+
+        public void Restore(Memento m)
+        {
+            balance = m.Balance;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(balance)}: {balance}";
+        }
+    }
+}
---
+++ Behavioral.Memento/Memento.cs
@@ -0,0 +1,12 @@
+namespace Behavioral.Memento
+{
+    public class Memento
+    {
+        public int Balance { get; }
+
+        public Memento(int balance)
+        {
+            Balance = balance;
+        }
+    }
+}
