f517565fffa920cbb581620d7912ba8d8b5c7753
AndyBrizhak <andrewbrizhak@gmail.com>
2019-08-04 10:38:18 +0800

    +

---
+++ Behavioral.MediatrDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Autofac;
+//using DotNetDesignPatternDemos.Annotations;
+using MediatR;
+
+namespace MediatrDemo
+{
+  public class Demo
+  {
+    public static async Task Main()
+    {
+      var builder = new ContainerBuilder();
+      builder.RegisterType<Mediator>()
+        .As<IMediator>()
+        .InstancePerLifetimeScope(); // singleton
+
+      builder.Register<ServiceFactory>(context =>
+      {
+        var c = context.Resolve<IComponentContext>();
+        return t => c.Resolve(t);
+      });
+
+      builder.RegisterAssemblyTypes(typeof(Demo).Assembly)
+        .AsImplementedInterfaces();
+
+      var container = builder.Build();
+      var mediator = container.Resolve<IMediator>();
+      var response = await mediator.Send(new PingCommand());
+      Console.WriteLine($"We got a pong at {response.Timestamp}");
+    }
+  }
+}
---
+++ Behavioral.Memento/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+
+namespace Behavioral.Memento
+{
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            var ba = new BankAccount(100);
+            var m1 = ba.Deposit(50); // 150
+            var m2 = ba.Deposit(25); // 175
+            WriteLine(ba);
+
+            // restore to m1
+            ba.Restore(m1);
+            WriteLine(ba);
+
+            ba.Restore(m2);
+            WriteLine(ba);
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+}
