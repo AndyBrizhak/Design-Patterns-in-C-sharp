db131374d52ccfb239100039c3c43720b0875d61
AndyBrizhak <andrewbrizhak@gmail.com>
2020-03-13 14:51:54 +0800

    +

--- Bridge Coding Exercise/Square.cs
+++ Bridge Coding Exercise/Square.cs
@@ -3,7 +3,7 @@ namespace Bridge_Coding_Exercise
     public class Square: Shape
     {
         /// <summary>
-        /// ++++
+        /// ++++++
         /// </summary>
         public Square() => Name = "Square";
         public override void Draw()
