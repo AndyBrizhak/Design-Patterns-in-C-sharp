222735b56ca8b5854bb09f5d8c79410b1ff0d91f
AndyBrizhak <andrewbrizhak@gmail.com>
2020-06-13 07:33:37 +0800

    add pattern Combinator

--- SOLID.SRP/Demo.cs
+++ SOLID.SRP/Demo.cs
@@ -24,8 +24,35 @@ namespace SOLID.SRP
             foreach (var p in pf.FilterByColor(products, Color.Green))
                 WriteLine($" - {p.Name} is green");
 
+            // vv AFTER
+            var bf = new BetterFilter();
+            WriteLine("Green products (new):");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+                WriteLine($" - {p.Name} is green");
+
+
+            WriteLine("Large products");
+            foreach (var p in bf.Filter(products, new SizeSpecification(Size.Large)))
+                WriteLine($" - {p.Name} is large");
+
+            var largeGreenSpec = new ColorSpecification(Color.Green)
+                           & new SizeSpecification(Size.Large);
+            //var largeGreenSpec = Color.Green.And(Size.Large);
+
+            WriteLine("Large blue items");
+
+            foreach (var p in bf.Filter(products,
+                        new AndSpecification<Product>(new ColorSpecification(Color.Blue),
+                        new SizeSpecification(Size.Large))))
+            {
+                WriteLine($" - {p.Name} is big and blue");
+            }
+
+            Console.ReadKey();
 
         }
+
+
 
     }
 }
