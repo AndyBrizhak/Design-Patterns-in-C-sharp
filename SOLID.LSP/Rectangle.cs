e62f7f195ba6c4713776c8f31c3bc62b5e3b863e
AndyBrizhak <andrewbrizhak@gmail.com>
2020-06-15 10:26:44 +0800

    add example Liskov

---
+++ SOLID.LSP/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.LSP
+{
+    public class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Area => Width * Height;
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+}
