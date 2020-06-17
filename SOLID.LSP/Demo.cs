764fd8cdef1ca02904039ea44c21bd0fcf55ec4d
AndyBrizhak <andrewbrizhak@gmail.com>
2020-06-17 20:26:44 +0800

    add Demo example

---
+++ SOLID.LSP/Demo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SOLID.LSP
+{
+    public class Demo
+    {
+        public static void UseIt(Rectangle r)
+        {
+            int width = r.Width;
+            r.Height = 10;
+            WriteLine($"Expected area of {10 * width}, got {r.Area}");
+        }
+
+        static void Main()
+        {
+            var rc = new Rectangle(2, 3);
+            UseIt(rc);
+
+            var sq = new Square(5);
+            UseIt(sq);
+        }
+    }
+}
+
+   
